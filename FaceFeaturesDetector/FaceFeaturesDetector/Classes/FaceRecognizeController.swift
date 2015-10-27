127959947aedf2dab15bf51264ac192f71e0948d
Stas Kirichok <stanislav.kirichok@yalantis.com>
2015-10-27 19:52:48 +0100

    [Add] Added base UI components to controller

--- FaceFeaturesDetector/FaceFeaturesDetector/Classes/FaceRecognizeController.swift
+++ FaceFeaturesDetector/FaceFeaturesDetector/Classes/FaceRecognizeController.swift
@@ -8,18 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FaceRecognizeController: UIViewController {
+    
+    @IBOutlet private weak var photoView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func chooseImage() {
+        
     }
 
-
+    @IBAction func recognize() {
+        
+    }
 }
 
